2ccabfb94d7c8ea498763de252eaeeba4efba0c2
Mnico-11 <141962291+Mnico-11@users.noreply.github.com>
2025-11-17 17:54:48 -0500

    mu1

--- homework/pregunta_01.py
+++ homework/pregunta_01.py
@@ -1,15 +1,107 @@
 """
-Escriba el codigo que ejecute la accion solicitada en la pregunta.
+Escriba el codigo que ejecute la accion solicitada en cada pregunta.
 """
+
+# pylint: disable=import-outside-toplevel
+
+import os
+import pandas as pd
+import matplotlib.pyplot as plt
+from pathlib import Path
+
 
 
 def pregunta_01():
     """
-    Realice la limpieza del archivo "files/input/solicitudes_de_credito.csv".
-    El archivo tiene problemas como registros duplicados y datos faltantes.
-    Tenga en cuenta todas las verificaciones discutidas en clase para
-    realizar la limpieza de los datos.
+    Siga las instrucciones del video https://youtu.be/qVdwpxG_JpE para
+    generar el archivo `files/plots/news.png`.
 
-    El archivo limpio debe escribirse en "files/output/solicitudes_de_credito.csv"
+    Un ejemplo de la grafica final esta ubicado en la raíz de
+    este repo.
+
+    El gráfico debe salvarse al archivo `files/plots/news.png`.
 
     """
+    plt.Figure()
+
+    colors = {
+        "Television": "dimgray",
+        "Newspaper" : "grey",
+        "Internet"  : "tab:blue",
+        "Radio"     : "lightgrey"
+    }
+
+    zorder = {
+        'Television': 1,
+        'Newspaper' : 1,
+        'Internet'  : 2,
+        'Radio'     : 1,
+    }
+
+    linewidths = {
+        'Television': 2,
+        'Newspaper' : 2,
+        'Internet'  : 4,
+        'Radio'     : 2,
+    }
+
+    df = pd.read_csv("files/input/news.csv", sep = ",", index_col = 0)
+    for col in df.columns:
+        plt.plot(
+            df[col],
+            color = colors[col],
+            label = col,
+            zorder = zorder[col],
+            linewidth = linewidths[col],
+        )
+    
+    plt.title("How people get their news", fontsize = 16)
+    plt.gca().spines["top"].set_visible(False)
+    plt.gca().spines["left"].set_visible(False)
+    plt.gca().spines["right"].set_visible(False)
+    plt.gca().axes.get_yaxis().set_visible(False)
+
+    for col in df.columns:
+        first_year = df.index[0]
+        plt.scatter(
+            x = first_year,
+            y = df[col][first_year],
+            color = colors[col],
+            zorder = zorder[col],
+        )
+
+        plt.text(
+            first_year - 0.2,
+            df[col][first_year],
+            col + " " + str(df[col][first_year]) + "%",
+            ha = "right",
+            va = "center",
+            color = colors[col],
+        )
+
+        last_year = df.index[-1]
+        plt.scatter(
+            x = last_year,
+            y = df[col][last_year],
+            color = colors[col],
+        )
+        
+        plt.text(
+            last_year + 0.2,
+            df[col][last_year],
+            str(df[col][last_year]) + "%",
+            ha = "left",
+            va = "center",
+            color = colors[col],
+        )
+
+        plt.xticks(
+            ticks = df.index,
+            labels = df.index,
+            ha = "center"
+        )
+
+        plt.tight_layout()
+        os.makedirs("files/plots", exist_ok = True)
+        plt.savefig("files/plots/news.png")
+        plt.show()
